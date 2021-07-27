f6214dd4326986fc05160fdb4406a237c2abaeed
Jenner Dulce <jennerdulce@gmail.com>
2021-07-26 23:24:24 -0700

    New base File

--- src/components/Search/Add.js
+++ src/components/Search/Add.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import { makeStyles } from '@material-ui/core';
-import { useDispatch } from 'react-redux'
-import { addFriend } from '../../store/profile.js'
+import { useDispatch, useSelector } from 'react-redux'
+import { addFriend, removeFriend } from '../../store/profile.js'
 
 const useStyles = makeStyles((theme) => ({
   addButton: {
     fontSize: '2.5rem',
-    color: '#69b2f7'
+    color: '#69b2f7',
+    cursor: 'pointer'
+  },
+  removeButton: {
+    fontSize: '2.5rem',
+    color: '#f64c4c',
+    cursor: 'pointer'
   }
 }))
 
@@ -15,14 +22,24 @@ const useStyles = makeStyles((theme) => ({
 function Add(props) {
   const classes = useStyles()
   const dispatch = useDispatch()
+  const isFriend = useSelector((state) => state.profile.profile.friends)
 
   const addNewFriend = (user) => {
     dispatch(addFriend(user))
   }
 
+  const removePerson = (person) => {
+    dispatch(removeFriend(person.username))
+  }
+
   return (
     <div>
-      <AddCircleIcon onClick={() => addNewFriend(props.userObj)} className={classes.addButton} />
+      {
+        isFriend.includes(props.userObj.username) ?
+          <RemoveCircleIcon onClick={() => removePerson(props.userObj.username)} className={classes.removeButton} />
+          :
+          <AddCircleIcon onClick={() => addNewFriend(props.userObj.username)} className={classes.addButton} />
+      }
     </div>
   )
 }
